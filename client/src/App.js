8879a518faebf3ad827ca7d99e9c29e11c111e54
Ekas <ekaspreet99@gmail.com>
2023-12-17 19:52:57 +0530

    yoyoyo

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          yoyoyoyyoyoyoyo
         </p>
         <a
           className="App-link"
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Harshreet
+          Hello Harmeet
+          ekaspreet singh
         </a>
       </header>
     </div>
