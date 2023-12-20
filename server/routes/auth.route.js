fc95ea41de37ad185a2999d9d6e5428b2fbea45e
Ekas <ekaspreet99@gmail.com>
2023-12-20 23:43:36 +0530

    sign out functionality

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { google, signin, signup } from "../controllers/auth.controller.js";
+import { google, signout, signin, signup } from "../controllers/auth.controller.js";
+import { sign } from "crypto";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/google", google);
+router.get("/signout", signout)
 
 export default router;
