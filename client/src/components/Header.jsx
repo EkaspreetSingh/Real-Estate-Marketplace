ea70da5eac195b7e74c0f781357f519a032a2e75
Ekas <ekaspreet99@gmail.com>
2023-12-21 02:59:21 +0530

    rush

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,27 @@
 import { FaSearch } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  // const [searchTerm, setSearchTerm] = useState("");
-  // const navigate = useNavigate();
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   const urlParams = new URLSearchParams(window.location.search);
-  //   urlParams.set("searchTerm", searchTerm);
-  //   const searchQuery = urlParams.toString();
-  //   navigate(`/search?${searchQuery}`);
-  // };
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
 
-  // useEffect(() => {
-  //   const urlParams = new URLSearchParams(location.search);
-  //   const searchTermFromUrl = urlParams.get("searchTerm");
-  //   if (searchTermFromUrl) {
-  //     setSearchTerm(searchTermFromUrl);
-  //   }
-  // }, [location.search]);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -31,7 +31,7 @@ export default function Header() {
             <span className="text-slate-700">Sphere</span>
           </h1>
         </Link>
-        {/* <form
+        <form
           onSubmit={handleSubmit}
           className="bg-slate-100 p-3 rounded-lg flex items-center"
         >
@@ -45,7 +45,7 @@ export default function Header() {
           <button>
             <FaSearch className="text-slate-600" />
           </button>
-        </form> */}
+        </form>
         <ul className="flex gap-4">
           <Link to="/">
             <li className="hidden sm:inline text-slate-700 hover:underline">
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,10 +4,12 @@ export default function About() {
   return (
     <div className="py-20 px-4 max-w-6xl mx-auto">
       <h1 className="text-3xl font-bold mb-4 text-slate-800">
-        About LegacyLandmarks
+        About Estate
+Sphere
       </h1>
       <p className="mb-4 text-slate-700">
-        LegacyLandmarks is a leading real estate agency that specializes in
+        Estate
+Sphere is a leading real estate agency that specializes in
         helping clients buy, sell, and rent properties in the most desirable
         neighborhoods. Our team of experienced agents is dedicated to providing
         exceptional service and making the buying and selling process as smooth
