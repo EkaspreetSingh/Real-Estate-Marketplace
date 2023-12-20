a28dce31afbca6bb946d80ceb104ccb36d08a394
harsidaksingh <84435027+harsidaksingh@users.noreply.github.com>
2023-12-20 13:01:52 +0530

    Revert "signup route finished"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
@@ -11,15 +10,6 @@ import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
 import Search from "./pages/Search";
-=======
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Profile from './pages/Profile'
-import Header from './pages/About'
->>>>>>> Stashed changes
 
 export default function App() {
   return (
