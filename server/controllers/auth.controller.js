fead365a51fa08edf21669f91e773ce9aa3d4fc7
Ekas <ekaspreet99@gmail.com>
2023-12-20 15:28:29 +0530

    compeleted sign in functionality

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js'
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -22,10 +23,11 @@ export const signin = async (req, res, next) => {
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if(!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
         res
             .cookie('access_token', token, {httpOnly: true})
             .status(200)
-            .json(validUser);
+            .json(rest);
     }
     catch (error) {
         next(error);
