72edcc594d610558ac9b3c4fda74f0989d65a818
Ekas <ekaspreet99@gmail.com>
2023-12-20 14:49:07 +0530

    handle error and middleware plus basic signin work

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
@@ -9,7 +10,24 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json('user created successfully');
     } catch (error) {    
-        res.status(500).json({ message: error.message });
+        next(error);
     }
-    // req.status(201).json(newUser);
+};
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        res
+            .cookie('access_token', token, {httpOnly: true})
+            .status(200)
+            .json(validUser);
+    }
+    catch (error) {
+        next(error);
+    }
 }
