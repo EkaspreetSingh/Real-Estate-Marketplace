d11c6a611589ea48a5ea87cb8b74966b4108132e
harsidaksingh <harsidak.bindra21@gmail.com>
2024-01-12 21:33:49 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,20 +1,23 @@
 // Footer.js
 
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     <footer style={footerStyle}>
-      <p>&copy; 2023 Industrial Training Project</p>
+      <p>
+        &copy; 2023 Industrial Training Project by ekaspreet harmeet harshreet
+        harsidak
+      </p>
     </footer>
   );
 };
 
 const footerStyle = {
-  background: '#0f172a',
-  color: '#fff',
-  textAlign: 'center',
-  padding: '1rem',
+  background: "#0f172a",
+  color: "#fff",
+  textAlign: "center",
+  padding: "1rem",
 };
 
 export default Footer;
